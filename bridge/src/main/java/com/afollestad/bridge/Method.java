cae39d9a2edebd9f8601c3d33220cc7620b2fac6
Aidan Follestad <drummer.aidan@gmail.com>
2015-08-08 17:28:54 -0500

    Method now contains static members, it's no longer an enum. Enums are bad for performance and memory on Android.

--- bridge/src/main/java/com/afollestad/bridge/Method.java
+++ bridge/src/main/java/com/afollestad/bridge/Method.java
@@ -1,8 +1,29 @@
 package com.afollestad.bridge;
 
+import android.support.annotation.NonNull;
+
 /**
  * @author Aidan Follestad (afollestad)
  */
-public enum Method {
-    GET, PUT, POST, DELETE
+public final class Method {
+
+    private String mName;
+
+    private Method(@NonNull String name) {
+        mName = name;
+    }
+
+    public final static Method GET = new Method("GET");
+    public final static Method PUT = new Method("PUT");
+    public final static Method POST = new Method("POST");
+    public final static Method DELETE = new Method("DELETE");
+
+    public String name() {
+        return mName;
+    }
+
+    @Override
+    public String toString() {
+        return name();
+    }
 }
