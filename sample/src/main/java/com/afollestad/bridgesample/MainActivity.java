1a840b8c11ff9bd72a85cbc16ed13e40c49a49ce
Aidan Follestad <drummer.aidan@gmail.com>
2015-12-16 09:45:49 -0600

    Removed no-longer-used sample code

--- sample/src/main/java/com/afollestad/bridgesample/MainActivity.java
+++ sample/src/main/java/com/afollestad/bridgesample/MainActivity.java
@@ -1,17 +1,11 @@
 package com.afollestad.bridgesample;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.afollestad.bridge.Bridge;
-import com.afollestad.bridge.BridgeException;
-import com.afollestad.bridge.Response;
-import com.afollestad.bridge.ResponseConvertCallback;
-import com.afollestad.bridgesample.conversion.Person;
 
 /**
  * @author Aidan Follestad (afollestad)
@@ -26,14 +20,6 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView list = (RecyclerView) findViewById(R.id.list);
         list.setLayoutManager(new GridLayoutManager(this, 8));
         list.setAdapter(new MainAdapter());
-
-Bridge.get("https://www.someurl.com/test.js")
-    .asClass(Person.class, new ResponseConvertCallback<Person>() {
-        @Override
-        public void onResponse(@NonNull Response response, @Nullable Person object, @Nullable BridgeException e) {
-            // Use response object
-        }
-    });
     }
 
     @Override
