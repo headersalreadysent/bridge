be88b6f79fe12b502e702ea6d80331931f4d0f25
Aidan Follestad <drummer.aidan@gmail.com>
2017-01-24 20:07:42 -0600

    Update

--- library/src/main/java/com/afollestad/bridge/BasicAuthentication.java
+++ library/src/main/java/com/afollestad/bridge/BasicAuthentication.java
@@ -7,7 +7,8 @@ import java.util.Locale;
 /**
  * @author Aidan Follestad (afollestad)
  */
-@SuppressWarnings("WeakerAccess") public class BasicAuthentication implements Authentication {
+@SuppressWarnings({"WeakerAccess", "unused"})
+public class BasicAuthentication implements Authentication {
 
     private final static String HEADER_NAME = "Authorization";
 
